749cf658ed80386e68ac69e8902cdedab28cbd56
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-15 22:59:32 +0000

    Implemented: Displaying Basic Option List

--- LawFirm__SimpleSolution/MainRun.cs
+++ LawFirm__SimpleSolution/MainRun.cs
@@ -4,6 +4,13 @@ class MainRun
 {
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("Welcome to Law's Firm Management System");
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("[1] - Create Entry");
+        Console.WriteLine("[2] - Read Entry");
+        Console.WriteLine("[3] - Update Entry");
+        Console.WriteLine("[4] - Delete Entry");
+
+        int user_option = Convert.ToInt32(Console.ReadLine());
     }
 }
