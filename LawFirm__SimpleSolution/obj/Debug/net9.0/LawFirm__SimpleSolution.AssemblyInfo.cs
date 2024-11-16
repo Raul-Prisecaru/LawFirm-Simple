e46daa7ecd9c2817d13f90a1c46f0080ea6ce1c8
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-16 14:23:35 +0000

    System

--- LawFirm__SimpleSolution/obj/Debug/net9.0/LawFirm__SimpleSolution.AssemblyInfo.cs
+++ LawFirm__SimpleSolution/obj/Debug/net9.0/LawFirm__SimpleSolution.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LawFirm__SimpleSolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5751d1e15ef881026b3d4717a9e748b6fc56c2cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d848e90cfaf9353fee8acb3397ed0a535c698d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LawFirm__SimpleSolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LawFirm__SimpleSolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
