16f57fa628abc1575cc493163b14e363adbbff1b
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-16 00:16:05 +0000

    Implemented: Connection to the Database

--- LawFirm__SimpleSolution/Database/CreateDatabase.cs
+++ LawFirm__SimpleSolution/Database/CreateDatabase.cs
@@ -1,6 +1,16 @@
+using Microsoft.Data.Sqlite;
+
 namespace LawFirm__SimpleSolution.Database;
 
 public class CreateDatabase
 {
-    
+    void Database()
+    {
+        var databaseLocation = "Data Source=test.db";
+        using (var connection = new SqliteConnection(databaseLocation))
+        {
+            connection.Open();
+            Console.WriteLine("Connected to the Database");
+        }
+    }
 }
