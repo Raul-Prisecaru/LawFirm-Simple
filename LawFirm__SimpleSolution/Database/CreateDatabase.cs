05ba582d0c77399d16612123c90198957feb1f3d
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-16 13:32:11 +0000

    Implemented: method for creating Case Table

--- LawFirm__SimpleSolution/Database/CreateDatabase.cs
+++ LawFirm__SimpleSolution/Database/CreateDatabase.cs
@@ -5,7 +5,134 @@ namespace LawFirm__SimpleSolution.Database;
 public class CreateDatabase
 {
     // Method for creating Client Table
-    void CreateClients()
+    void CreateClientsTable()
+    {
+        // Error Handling
+        try
+        {
+            // Defining Location of the Database
+            var databaseLocation = "Data Source=LawFirmDatabase.db";
+            // Creating an instance of the db and Close the db once done
+            using (var connection = new SqliteConnection(databaseLocation))
+            {
+                // Connecting to the Database
+                connection.Open();
+                Console.WriteLine("Connected to the Database");
+                
+                // Instance of SQLite Command Object
+                var command = connection.CreateCommand();
+                
+                // SQL Query to Create Table
+                command.CommandText = @"
+                CREATE TABLE IF NOT EXISTS clients (
+                    client_id INTEGER PRIMARY KEY,
+                    client_name TEXT NOT NULL,
+                    client_address TEXT,
+                    client_phone TEXT,
+                    client_emai TEXT
+                ) ";
+                
+                // Executing Query that is not expected to return results
+                command.ExecuteNonQuery();
+            
+                Console.WriteLine("Created Client Table");
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("There has been an error that occured: " + e);
+        }
+
+    }
+    
+    void CreateCasesTable()
+    {
+        // Error Handling
+        try
+        {
+            // Defining Location of the Database
+            var databaseLocation = "Data Source=LawFirmDatabase.db";
+            // Creating an instance of the db and Close the db once done
+            using (var connection = new SqliteConnection(databaseLocation))
+            {
+                // Connecting to the Database
+                connection.Open();
+                Console.WriteLine("Connected to the Database");
+                
+                // Instance of SQLite Command Object
+                var command = connection.CreateCommand();
+                
+                // SQL Query to Create Table
+                command.CommandText = @"
+                CREATE TABLE IF NOT EXISTS clients (
+                    case_id INTEGER PRIMARY KEY,
+                    case_number TEXT UNIQUE,
+                    case_title TEXT NOT NULL,
+                    case_description TEXT,
+                    case_status TEXT,
+                    date_filed DATE,
+                    date_closed DATE,
+                    client_id INTEGER
+                    FOREIGN KEY (client_id) REFERENCES clients(client_id) ON DELETE SET NULL
+                ) ";
+                
+                // Executing Query that is not expected to return results
+                command.ExecuteNonQuery();
+            
+                Console.WriteLine("Created Cases Table");
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("There has been an error that occured: " + e);
+        }
+
+    }
+    
+    void CreateLegalDocumentsTable()
+    {
+        // Error Handling
+        try
+        {
+            // Defining Location of the Database
+            var databaseLocation = "Data Source=LawFirmDatabase.db";
+            // Creating an instance of the db and Close the db once done
+            using (var connection = new SqliteConnection(databaseLocation))
+            {
+                // Connecting to the Database
+                connection.Open();
+                Console.WriteLine("Connected to the Database");
+                
+                // Instance of SQLite Command Object
+                var command = connection.CreateCommand();
+                
+                // SQL Query to Create Table
+                command.CommandText = @"
+                CREATE TABLE IF NOT EXISTS clients (
+                    client_id INTEGER PRIMARY KEY,
+                    client_name TEXT NOT NULL,
+                    client_address TEXT,
+                    client_phone TEXT,
+                    client_emai TEXT
+                ) ";
+                
+                // Executing Query that is not expected to return results
+                command.ExecuteNonQuery();
+            
+                Console.WriteLine("Created Client Table");
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("There has been an error that occured: " + e);
+        }
+
+    }
+    
+    void CreateDatesTable()
     {
         // Error Handling
         try
