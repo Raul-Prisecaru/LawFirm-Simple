0d838e84672e04b99c6ef0b3a0f31ba44a668293
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-16 00:25:35 +0000

    Implemented: method for creating Client Table

--- LawFirm__SimpleSolution/Database/CreateDatabase.cs
+++ LawFirm__SimpleSolution/Database/CreateDatabase.cs
@@ -4,13 +4,27 @@ namespace LawFirm__SimpleSolution.Database;
 
 public class CreateDatabase
 {
-    void Database()
+    void CreateClients()
     {
         var databaseLocation = "Data Source=test.db";
         using (var connection = new SqliteConnection(databaseLocation))
         {
             connection.Open();
             Console.WriteLine("Connected to the Database");
+
+            var createCommand = connection.CreateCommand();
+
+            createCommand.CommandText = @"
+                CREATE TABLE IF NOT EXISTS clients (
+                    client_id INTEGER PRIMARY KEY,
+                    client_name TEXT NOT NULL,
+                    client_address TEXT,
+                    client_phone TEXT,
+                    client_emai TEXT
+                ) ";
+            createCommand.ExecuteNonQuery();
+            
+            Console.WriteLine("Created Client Table");
         }
     }
 }
