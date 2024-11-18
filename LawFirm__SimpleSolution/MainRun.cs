f77dc51d2cf341a84620e04651f46478ddf21fcf
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-18 20:28:16 +0000

    Implemented: Basic Input to allow insert of Data to the DB

--- LawFirm__SimpleSolution/MainRun.cs
+++ LawFirm__SimpleSolution/MainRun.cs
@@ -19,13 +19,92 @@ class MainRun
         Console.WriteLine("[4] - Delete Entry");
 
         int user_option = Convert.ToInt32(Console.ReadLine());
-
+        createDatabase.createTables();
         switch (user_option)
         {
             case 1:
                 Console.WriteLine("You have Chosen to Create Entry");
-                createDatabase.createTables();
-                insertData.InsertClientsTable("Test", "test", "test", "test");
+                Console.WriteLine("Which data do you want to Create?");
+                Console.WriteLine("[1] - Clients");
+                Console.WriteLine("[2] - Cases");
+                Console.WriteLine("[3] - Documents");
+                Console.WriteLine("[4] - Dates");
+                int userOptionCreate = Convert.ToInt32(Console.ReadLine());
+                switch (userOptionCreate)
+                {
+                    case 1:
+                        Console.Write("Client Name: ");
+                        String clientNameInput = Console.ReadLine();
+
+                        Console.Write("Client Address: ");
+                        String clientAddressInput = Console.ReadLine();
+
+                        Console.Write("Client Phone Number: ");
+                        String clientPhoneInput = Console.ReadLine();
+
+                        Console.Write("Client Email: ");
+                        String clientEmailInput = Console.ReadLine();
+
+
+                        insertData.InsertClientsTable(clientNameInput, clientAddressInput, clientPhoneInput, clientEmailInput);
+                        break;
+
+                    case 2:
+                        Console.Write("Case Number: ");
+                        String caseNumberInput = Console.ReadLine();
+
+                        Console.Write("Case Title: ");
+                        String caseTitleInput = Console.ReadLine();
+
+                        Console.Write("Case Description: ");
+                        String caseDescriptionInput = Console.ReadLine();
+
+                        Console.Write("Case Status: ");
+                        String caseStatusInput = Console.ReadLine();
+
+                        Console.Write("Date Filed: ");
+                        String caseDateFiledInput = Console.ReadLine();
+                        DateTime convertDateFiledInput = DateTime.Parse(caseDateFiledInput);
+
+                        Console.Write("Date Closed: ");
+                        String caseDateClosedInput = Console.ReadLine();
+
+                        Console.Write("Client ID: ");
+                        String caseClientIDInput = Console.ReadLine();
+
+                        insertData.InsertCasesTable(caseNumberInput, caseTitleInput, caseDescriptionInput, caseStatusInput, caseDateFiledInput, caseDateClosedInput, caseClientIDInput);
+                        break;
+
+                    case 3:
+                        Console.Write("Case ID: ");
+                        Int32 documentCaseIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Document Name: ");
+                        String documentNameInput = Console.ReadLine();
+
+                        Console.Write("Document Type: ");
+                        String documentTypeInput = Console.ReadLine();
+
+                        Console.Write("Document Path: ");
+                        String documentPathInput = Console.ReadLine();
+
+                        insertData.InsertDocumentTable(documentCaseIDInput, documentNameInput, documentTypeInput, documentPathInput);
+                        break;
+
+                    case 4:
+                        Console.Write("Case ID: ");
+                        Int32 dateCaseIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Event Date: ");
+                        String dateEventDateInput = Console.ReadLine();
+
+                        Console.Write("Event Description: ");
+                        String dateEventDescriptionInput = Console.ReadLine();
+
+
+                        insertData.InsertDateTable(dateCaseIDInput, dateEventDateInput, dateEventDescriptionInput);
+                        break;
+                }
                 break;
             case 2:
                 Console.WriteLine("You have Chosen to Read Entry");
@@ -54,7 +133,6 @@ class MainRun
                         readDatabase.ReadDateTable();
                         break;
                 }
-                readDatabase.ReadClientsTable();
                 break;
             
             case 3:
