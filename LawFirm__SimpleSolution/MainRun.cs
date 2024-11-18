27d94e2bb8ae95aabdf123f32c926709e2d78341
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-18 20:51:07 +0000

    Implemented: Basic input for Updating and Deleting data from tables in DB

--- LawFirm__SimpleSolution/MainRun.cs
+++ LawFirm__SimpleSolution/MainRun.cs
@@ -9,6 +9,8 @@ class MainRun
         CreateDatabase createDatabase = new CreateDatabase();
         InsertDatabase insertData = new InsertDatabase();
         ReadDatabase readDatabase = new ReadDatabase();
+        UpdateDatabase updateDatabase = new UpdateDatabase();
+        DeleteDatabase deleteDatabase = new DeleteDatabase();
         createDatabase.createTables();
         
         Console.WriteLine("Welcome to Law's Firm Management System");
@@ -137,10 +139,138 @@ class MainRun
             
             case 3:
                 Console.WriteLine("You have chosen to Update Entry");
+                Console.WriteLine("Which data do you want to Update?");
+                Console.WriteLine("[1] - Clients");
+                Console.WriteLine("[2] - Cases");
+                Console.WriteLine("[3] - Documents");
+                Console.WriteLine("[4] - Dates");
+                int userOptionUpdate = Convert.ToInt32(Console.ReadLine());
+                switch (userOptionUpdate)
+                {
+                    case 1:
+                        Console.Write("Client ID to Update: ");
+                        Int32 clientIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Client Name: ");
+                        String clientNameInput = Console.ReadLine();
+
+                        Console.Write("Client Address: ");
+                        String clientAddressInput = Console.ReadLine();
+
+                        Console.Write("Client Phone Number: ");
+                        String clientPhoneInput = Console.ReadLine();
+
+                        Console.Write("Client Email: ");
+                        String clientEmailInput = Console.ReadLine();
+
+
+                        updateDatabase.UpdateClientsTable(clientIDInput, clientNameInput, clientAddressInput, clientPhoneInput, clientEmailInput);
+                        break;
+
+                    case 2:
+                        Console.Write("Case ID to update: ");
+                        Int32 caseIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Case Number: ");
+                        String caseNumberInput = Console.ReadLine();
+
+                        Console.Write("Case Title: ");
+                        String caseTitleInput = Console.ReadLine();
+
+                        Console.Write("Case Description: ");
+                        String caseDescriptionInput = Console.ReadLine();
+
+                        Console.Write("Case Status: ");
+                        String caseStatusInput = Console.ReadLine();
+
+                        Console.Write("Date Filed: ");
+                        String caseDateFiledInput = Console.ReadLine();
+                        DateTime convertDateFiledInput = DateTime.Parse(caseDateFiledInput);
+
+                        Console.Write("Date Closed: ");
+                        String caseDateClosedInput = Console.ReadLine();
+
+                        Console.Write("Client ID: ");
+                        String caseClientIDInput = Console.ReadLine();
+
+                        updateDatabase.UpdateCasesTable(caseIDInput, caseNumberInput, caseTitleInput, caseDescriptionInput, caseStatusInput, caseDateFiledInput, caseDateClosedInput, caseClientIDInput);
+                        break;
+
+                    case 3:
+                        Console.Write("Document ID to update: ");
+                        Int32 documentDocumentIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Case ID: ");
+                        Int32 documentCaseIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Document Name: ");
+                        String documentNameInput = Console.ReadLine();
+
+                        Console.Write("Document Type: ");
+                        String documentTypeInput = Console.ReadLine();
+
+                        Console.Write("Document Path: ");
+                        String documentPathInput = Console.ReadLine();
+
+                        updateDatabase.UpdateDocumentTable(documentDocumentIDInput, documentCaseIDInput, documentNameInput, documentTypeInput, documentPathInput);
+                        break;
+
+                    case 4:
+                        Console.Write("Date ID to update: ");
+                        Int32 dateDateIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Case ID: ");
+                        Int32 dateCaseIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Event Date: ");
+                        String dateEventDateInput = Console.ReadLine();
+
+                        Console.Write("Event Description: ");
+                        String dateEventDescriptionInput = Console.ReadLine();
+
+                        updateDatabase.UpdateDateTable(dateDateIDInput,dateCaseIDInput, dateEventDateInput, dateEventDescriptionInput);
+                        break;
+                }
                 break;
             
             case 4:
                 Console.WriteLine("You have chosen to Delete Entry");
+                Console.WriteLine("Which data do you want to Delete?");
+                Console.WriteLine("[1] - Clients");
+                Console.WriteLine("[2] - Cases");
+                Console.WriteLine("[3] - Documents");
+                Console.WriteLine("[4] - Dates");
+                int userOptionDelete = Convert.ToInt32(Console.ReadLine());
+                switch (userOptionDelete)
+                {
+                    case 1:
+                        Console.Write("Client ID to Delete: ");
+                        Int32 clientIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        deleteDatabase.DeleteClientsTable(clientIDInput);
+                        break;
+
+                    case 2:
+                        Console.Write("Case ID to update: ");
+                        Int32 caseIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        deleteDatabase.DeleteCasesTable(caseIDInput);
+                        break;
+
+                    case 3:
+                        Console.Write("Document ID to update: ");
+                        Int32 documentDocumentIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        deleteDatabase.DeleteDocumentTable(documentDocumentIDInput);
+                        break;
+
+                    case 4:
+                        Console.Write("Date ID to update: ");
+                        Int32 dateDateIDInput = Convert.ToInt32(Console.ReadLine());
+
+                        deleteDatabase.DeleteDateTable(dateDateIDInput);
+                        break;
+                }
                 break;
         }
         
